5f9bffa7b68eae3abfe915ca6ff8f52d6d9dacff
nazary001 <nazary9911@gmail.com>
2022-08-31 22:29:56 +0300

    Doned

--- src/App.js
+++ src/App.js
@@ -2,23 +2,15 @@ import React from "react";
 import Logo from "./Logo";
 import BestPokemon from "./BestPokemon";
 import CaughtPokemon from "./CaughtPokemon";
-//import PokemonMoves from "./PokemonMoves";
-
 
 function App() {
-  function logWhenClicked() {
-  console.log("look at this button, working and stuff!");
-}
   return (
-  <div>
-    <Logo appName="Pokedex" handleClick={logWhenClicked} />
+  <div id="div1">
+    <Logo appName = "Pokedex"/>
     <BestPokemon abilities = {["Anticipation", "Adaptability", "Run-Away"]}/>
     <CaughtPokemon date = {new Date().toLocaleDateString()}/>
   </div>
   );
 }
 
-
 export default App;
-
-
